ea26a1af6c084fc13a0702917b1a618a5a733548
Felipe Pedroso <f.pedroso677@gmail.com>
2017-08-21 23:25:09 -0300

    Creatig histogram for each image.

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ import os
 import datetime
 from PIL import Image
 import glob
+import cv2
+from sklearn.cluster import KMeans
 
 timeStarted = datetime.datetime.utcnow()
 
 imagesPathsArray = glob.glob("coil-100" + os.path.sep + "*.png")
 
-allImagesArray = []
+imagesHistograms = {}
 
 for imagePath in imagesPathsArray:
-    image = Image.open(imagePath)
-    rgbMatrix = np.asarray(image)
-    imageVector = rgbMatrix.reshape(128*128, -1)
-    allImagesArray.append(imageVector)
+    image = cv2.imread(imagePath)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+    clt = KMeans(n_clusters=5)
+    clt.fit(image)
 
-print(allImagesArray)
-print(len(allImagesArray))
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (imageHistogram, _) = np.histogram(clt.labels_, bins=numLabels)
+    imageHistogram = imageHistogram.astype("float")
+    imageHistogram /= imageHistogram.sum()
+
+    imagesHistograms[imagePath] = imageHistogram
+
 
 timeEnded = datetime.datetime.utcnow()
 print("Rodou em: " + str((timeEnded - timeStarted).total_seconds()))
